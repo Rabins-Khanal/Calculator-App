26d464c7eaba6ae75d7ecf453be90d3da7adf4e6
Rabins-Khanal <sjlkhanal@gmail.com>
2024-12-10 11:31:34 +0545

    calculator app

---
+++ android/app/src/main/kotlin/com/rabins/calculator_app/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.rabins.calculator_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/app.dart
@@ -0,0 +1,17 @@
+import 'package:calculator_app/view/calculator_view.dart';
+import 'package:flutter/material.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      initialRoute: '/',
+      debugShowCheckedModeBanner: false,
+      routes: {
+        '/': (context) => const CalculatorView(),
+      },
+    );
+  }
+}
